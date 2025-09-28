5af6c60f4659636bf7e16451d40f7db2fe283f05
swati <swatisharmaofficial04@gmail.com>
2025-09-28 20:03:20 +0530

    commit7

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -24,7 +24,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients    
+  // io.emit() is used to send events to all the connected clients     
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
