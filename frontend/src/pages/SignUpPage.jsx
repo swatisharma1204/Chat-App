30f74723e6d54740c18ee942de708237cf30f3f0
swati <swatisharmaofficial04@gmail.com>
2025-10-02 15:03:20 +0530

    commit3

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom"; 
 
-import AuthImagePattern from "../components/AuthImagePattern";  
+import AuthImagePattern from "../components/AuthImagePattern";   
 import toast from "react-hot-toast"; 
 
 const SignUpPage = () => {
