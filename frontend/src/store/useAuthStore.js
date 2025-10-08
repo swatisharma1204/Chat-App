66b38fdccd9d79c46d5deb3a0bfbc0bc965c4c2e
swati <swatisharmaofficial04@gmail.com>
2025-10-08 08:47:50 +0530

    commit7

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -47,7 +47,7 @@ export const useAuthStore = create((set, get) => ({
     try { 
       const res = await axiosInstance.post("/auth/login", data);  
       set({ authUser: res.data });
-      toast.success("Logged in successfully"); 
+      toast.success("Logged in successfully");  
  
       get().connectSocket(); 
     } catch (error) {
