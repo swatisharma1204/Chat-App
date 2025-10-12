921a3755b68a2accd400958d5e8fd3f42cab693d
swati <swatisharmaofficial04@gmail.com>
2025-10-12 19:01:45 +0530

    commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  const MONGODB_URI= "mongodb://localhost:27017/ChatApp"
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
