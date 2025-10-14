701e4718fc6c7ffdffc8b95b1c29707f1d44ab1d
swati <swatisharmaofficial04@gmail.com>
2025-10-14 09:25:22 +0530

    commit4

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
   cors({  
     origin: "http://localhost:5173",
-    credentials: true,
+    credentials: true, 
      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
