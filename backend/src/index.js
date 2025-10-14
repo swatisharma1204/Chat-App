7b104d537d712c7c985e7c74c50abe4de30eaeb1
swati <swatisharmaofficial04@gmail.com>
2025-10-14 09:25:33 +0530

    commit5

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
  
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(
+app.use( 
   cors({  
     origin: "http://localhost:5173",
     credentials: true, 
