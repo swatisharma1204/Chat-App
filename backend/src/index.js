135774a28df0aacbf8dbefb69c6c36f420006b45
swati <swatisharmaofficial04@gmail.com>
2025-10-14 09:25:43 +0530

    commit6

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(
   cors({  
     origin: "http://localhost:5173",
     credentials: true, 
-     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
