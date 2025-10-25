d16ab13067a14d56de1883d4483c4c844f9c4c6e
swati <swatisharmaofficial04@gmail.com>
2025-10-26 01:44:30 +0530

    commit8

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -73,7 +73,7 @@ export const useAuthStore = create((set, get) => ({
     try {
       const res = await axiosInstance.put("/auth/update-profile", data);
       set({ authUser: res.data });
-      toast.success("Profile updated successfully");
+      toast.success("Profile updated successfully"); 
     } catch (error) { 
       console.log("error in update profile:", error);
       toast.error(error.response.data.message); 
