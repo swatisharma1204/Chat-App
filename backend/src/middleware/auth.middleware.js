a6ab9903fd7d4ad2197aff60fbc5a9de7ee32e1e
swati <swatisharmaofficial04@gmail.com>
2025-11-19 21:54:59 +0530

    commit5

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -17,7 +17,7 @@ export const protectRoute = async (req, res, next) => {
 
     const user = await User.findById(decoded.userId).select("-password"); 
 
-    if (!user) {
+    if (!user) { 
       return res.status(404).json({ message: "User not found" }); 
     }
 
