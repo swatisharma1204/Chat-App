92307361e407266b9ad76361de0cc8ef1849b47d
swati <swatisharmaofficial04@gmail.com>
2025-11-19 21:54:20 +0530

    commit3

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -12,7 +12,7 @@ export const protectRoute = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET); 
 
     if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" }); 
     }
 
     const user = await User.findById(decoded.userId).select("-password");
